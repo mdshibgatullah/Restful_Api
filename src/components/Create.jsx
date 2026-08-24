99d300f7664af0d716ec54330530c1001eee229c
mdshibgatullah <mdshibgatullah94@gmail.com>
2026-08-24 22:21:55 +0600

    update

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,44 +1,36 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Header from './Header'
-import { Button, Container, Form, Row } from 'react-bootstrap'
+import { Container, Row } from 'react-bootstrap'
+import { useForm } from 'react-hook-form'
+import { useNavigate } from 'react-router-dom'
 
 const Create = () => {
+  const navigate = useNavigate()
 
-  const [formData, seFormData] = useState({
-    title : '',
-    description: '',
-    image : ''
-  })
-
-  const handleChange = ()=>{
-    seFormData({
-      ...formData,
-      [e.target.name] : e.target.value
-    })
-  }
-
-  const handleSubmit = ()=>{
-    e.preventDefault();
-    console.log(formData);
-    alert('Form submitted')
-  }
+const {
+  register,
+  handleSubmit,
+  formState: {errors}
+} = useForm()
 
 
-  const saveProduct = async ()=> {
+  const saveProduct = async (data)=> {
     const res = await fetch(`http://127.0.0.1:8000/api/product/`, {
       method: 'POST',
       headers: {
         'content-type' : 'application/json',
         'Accept' : 'application/json'
-      }
-    }).then(res = res.json())
+      },
+      body: JSON.stringify(data)
+    }).then(res => res.json(data))
     .then(result=>{
       if(result.status === 200){
-        alert("Product Created Successfully");
+        navigate('/');
       }
     })
 
   }
+
 
 
   return (
@@ -50,7 +42,7 @@ const Create = () => {
           <Row className='justify-content-center'>
             <div className="col-md-6 mt-5">
               <div className="card shadow">
-                <form action="" onSubmit={handleSubmit}>
+                <form action="" onSubmit={handleSubmit(saveProduct)}>
                   <div className="card-header bg-primary text-white">
                     <h2>Create Product</h2>
                   </div>
@@ -59,19 +51,30 @@ const Create = () => {
 
                     <div className='mb-3'>
                       <label for="title" className='form-label'>Title</label>
-                      <input onChange={handleChange} value={formData.title}
+                      <input 
+                      {
+                        ...register('title', 
+                          {required: 'Title field is required'}
+                        )
+                      }
                        type="text" className='form-control' id='title' name='title' placeholder='enter title' required/>
                     </div>
 
                     <div className='mb-3'>
                       <label for="description" className='form-label'>Description</label>
-                      <input onChange={handleChange} value={formData.description}
+                      <input 
+                      
+                        {
+                        ...register('description', 
+                          {required: 'Description field is required'}
+                         )
+                        }
                        type="description" className='form-control' id='description' name='description' placeholder='enter description' required/>
                     </div>
 
                     <div className='mb-3'>
                       <label for="file" className='form-label'>File</label>
-                      <input onChange={handleChange} value={formData.file}
+                      <input 
                       type="file" className='form-control' id='file' name='file'/>
                     </div>
 
