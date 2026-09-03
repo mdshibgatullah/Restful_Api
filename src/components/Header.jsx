2933f0eece2f134977d1ac0c281c95dc34a6659a
mdshibgatullah <mdshibgatullah94@gmail.com>
2026-09-03 12:23:52 +0600

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,35 @@
-import React from 'react'
-import { Container } from 'react-bootstrap'
+import React, { useState } from 'react';
+import { Container } from 'react-bootstrap';
+import { FaTimes, FaBars } from 'react-icons/fa';
+
 
 const Header = () => {
+  const [open, setOpen] = useState(false);
+
   return (
-    <header>
+    <header className="header">
       <Container>
-        <div className='d-flex justify-content-between align-items-center py-4'>
-          <div className="logo"><h2>Logo</h2></div>
-          <nav>
-            <ul className='d-flex gap-4 nav-link'>
-              <li><a href="/"></a>Home</li>
-              <li><a href="/"></a>About</li>
-              <li><a href="/"></a>Service</li>
-              <li><a href="/"></a>contact</li>
-            </ul>
-          </nav>
+        <div className="d-flex justify-content-between align-items-center py-4 position-relative">
+          <div className="logo">
+            <h2>Logo</h2>
+          </div>
+
+          <ul className='nav-link d-flex gap-4'>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Service</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+
+          <button
+          onClick={()=>setOpen(!open)}
+          >
+            {open ? <FaTimes /> : <FaBars />}
+          </button>
         </div>
       </Container>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
